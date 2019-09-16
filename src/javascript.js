4a0a85b898460876d47e959115de834b493717f7
Daido Shota <35087200+daido1976@users.noreply.github.com>
2019-09-16 13:26:20 +0900

    Update javascript.js

--- src/javascript.js
+++ src/javascript.js
@@ -2,24 +2,24 @@ let test = document.getElementById("hoge");
   
 // This handler will be executed only once when the cursor
 // moves over the unordered list
-test.addEventListener("mouseenter", function( event ) {   
+test.addEventListener("mouseenter", event => {   
   // highlight the mouseenter target
   event.target.style.color = "red";
 
   // reset the color after a short delay
-  setTimeout(function() {
+  setTimeout(() => {
     event.target.style.color = "";
   }, 500);
 }, false);
 
 // This handler will be executed every time the cursor
 // is moved over a different list item
-test.addEventListener("click", function( event ) {   
+test.addEventListener("click", event => {   
   // highlight the mouseover target
   event.target.style.color = "green";
 
   // reset the color after a short delay
-  setTimeout(function() {
+  setTimeout(() => {
     event.target.style.color = "";
   }, 500);
 }, false);
