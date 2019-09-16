6af0e6929ada711ec6e2c6e4eb5935c8c0e8e944
Daido Shota <35087200+daido1976@users.noreply.github.com>
2019-09-16 13:24:46 +0900

    Update javascript.js

--- src/javascript.js
+++ src/javascript.js
@@ -4,7 +4,7 @@ let test = document.getElementById("hoge");
 // moves over the unordered list
 test.addEventListener("mouseenter", function( event ) {   
   // highlight the mouseenter target
-  event.target.style.color = "white";
+  event.target.style.color = "red";
 
   // reset the color after a short delay
   setTimeout(function() {
