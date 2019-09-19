910bc5f5ae194e33733a63388e7450a65d341a35
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-09-20 00:16:15 +0900

    Impl changeColorWithHover & changeColorWithClick & moveElement

--- src/javascript.js
+++ src/javascript.js
@@ -1,34 +1,53 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event#JavaScript
 let element = document.getElementById("hoge");
 
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-element.addEventListener(
-  "mouseenter",
-  event => {
-    // highlight the mouseenter target
-    event.target.style.color = "red";
+const changeColorWithHover = () => {
+  element.addEventListener(
+    "mouseenter",
+    event => {
+      // highlight the mouseenter target
+      event.target.style.backgroundColor = "red";
 
-    // reset the color after a short delay
-    setTimeout(() => {
-      event.target.style.color = "";
-    }, 500);
-  },
-  false
-);
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.backgroundColor = "blue";
+      }, 500);
+    },
+    false
+  );
+};
 
-// This handler will be executed every time the cursor
-// is moved over a different list item
-element.addEventListener(
-  "click",
-  event => {
-    // highlight the mouseover target
-    event.target.style.color = "green";
+const changeColorWithClick = () => {
+  element.addEventListener(
+    "click",
+    event => {
+      // highlight the mouseover target
+      event.target.style.backgroundColor = "green";
 
-    // reset the color after a short delay
-    setTimeout(() => {
-      event.target.style.color = "";
-    }, 500);
-  },
-  false
-);
+      // reset the color after a short delay
+      setTimeout(() => {
+        event.target.style.backgroundColor = "blue";
+      }, 500);
+    },
+    false
+  );
+};
+
+const moveElement = () => {
+  document.addEventListener("mousedown", () => {
+    document.addEventListener("mousemove", onMouseMove);
+
+    document.addEventListener("mouseup", () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    });
+  });
+
+  const onMouseMove = e => {
+    element.style.left = `${e.clientX}px`;
+    element.style.top = `${e.clientY}px`;
+  };
+};
+
+moveElement();
+changeColorWithClick();
+changeColorWithHover();
